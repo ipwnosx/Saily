97407933d3fd2748dc4efb6fc475843317462304
Lakr Sakura <Lakr_Sakura@icloud.com>
2019-04-13 22:52:29 +0800

    Finally, init commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# SailyPackageManager
+The most magic package manger forever.  
+
+Will be open source after first public release.
---
+++ SailyPackageManager/Saily Package Manager/installedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Saily Package Manager
+//
+//  Created by Lakr Aream on 2019/4/13.
+//  Copyright © 2019 Lakr Aream. All rights reserved.
+//
+
+import UIKit
+
+class installedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
