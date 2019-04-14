2ee301ccd8329d524761aa618c1fb9a98fe15117
Lakr Sakura <Lakr_Sakura@icloud.com>
2019-04-15 02:57:22 +0800

    Clean Swift Logs

--- SailyPackageManager/Saily Package Manager/Codes/AppDelegate.swift
+++ SailyPackageManager/Saily Package Manager/Codes/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-//        SailyBridgerOBJCObjectInitED.redirectConsoleLogToDocumentFolder()
+        SailyBridgerOBJCObjectInitED.redirectConsoleLogToDocumentFolder()
         
         let homeDir = NSHomeDirectory()
         if (homeDir.contains("/Applications")) {
