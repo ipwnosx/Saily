dfeeed4609c5edd3c724feadfcdfdb4330fdcc54
Lakr Sakura <Lakr_Sakura@icloud.com>
2019-04-14 16:54:47 +0800

    init daemon

--- SailyPackageManager/SailyStartupDaemon/main.cpp
+++ SailyPackageManager/SailyStartupDaemon/main.cpp
@@ -8,11 +8,12 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Started Saily Package Manager Startup Daemon.\n";
+    
 	return 0;
 }
 
