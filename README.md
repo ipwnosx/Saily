f7bdff7ff1544ab61494cd31e539ab5ccba6ea79
Lakr Sakura <Lakr_Sakura@icloud.com>
2019-04-14 16:49:30 +0800

    Add Guard Daemon

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 The most magic package manger forever.  
 
 Will be open source after first public release.
+
+Require MonkeyDev
---
+++ SailyPackageManager/SailyStartupDaemon/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SailyStartupDaemon
+//
+//  Created by Lakr Aream on 2019/4/14.
+//  Copyright (c) 2019 Lakr Aream. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+	return 0;
+}
+
