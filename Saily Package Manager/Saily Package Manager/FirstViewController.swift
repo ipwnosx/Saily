a79e05064ab215eee4ab6378a7fcadb09e1bbceb
Lakr Sakura <Lakr_Sakura@icloud.com>
2019-04-19 19:01:01 +0800

    build NAN - reSubmit

---
+++ Saily Package Manager/Saily Package Manager/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Saily Package Manager
+//
+//  Created by Lakr Aream on 2019/4/19.
+//  Copyright © 2019 Lakr233. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
